8c905d94dfdaf01dbe4ba951e8ecd5b8a443aa91
arfullight <tmimvhuang@gmail.com>
2017-06-29 16:18:48 +0800

    course 38

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
   def homepage
-    session[:message] = '我會一直存在喔！'
-    render json: session
   end
 
   def about
-    render json: session
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
 
   get  '/login' => 'sessions#new', as: 'login'
   post '/login' => 'sessions#create'
+
+  delete '/logout' => 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
